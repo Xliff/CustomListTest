1e7ce3885b2a3cbc872b4d52bc70dea731e8e6e2
Clifton Wood <clifton.wood@gmail.com>
2015-08-16 23:10:00 -0400

    - Implement equals() and hashCode() for PlayerData objects.

--- app/src/main/java/com/example/cliff/customlisttest/data/PlayerData.java
+++ app/src/main/java/com/example/cliff/customlisttest/data/PlayerData.java
@@ -21,4 +21,34 @@ public class PlayerData {
         this.rank = rank;
         this.team = team;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PlayerData)) return false;
+
+        PlayerData that = (PlayerData) o;
+
+        if (bye != that.bye) return false;
+        if (rank != that.rank) return false;
+        if (!pos.equals(that.pos)) return false;
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null)
+            return false;
+        if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null)
+            return false;
+
+        return !(team != null ? !team.equals(that.team) : that.team != null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = pos.hashCode();
+        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        result = 31 * result + (team != null ? team.hashCode() : 0);
+        result = 31 * result + bye;
+        result = 31 * result + rank;
+
+        return result;
+    }
 }
